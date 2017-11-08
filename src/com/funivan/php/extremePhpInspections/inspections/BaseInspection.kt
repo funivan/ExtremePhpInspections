231d617348ee90e4060dfe1cd94621612da064b4
Ivan Scherbak <dev@funivan.com>
2017-11-08 13:04:49 +0200

    Remove unused class

--- src/com/funivan/php/extremePhpInspections/inspections/BaseInspection.kt
+++ src/com/funivan/php/extremePhpInspections/inspections/BaseInspection.kt
@@ -1,9 +1,6 @@
 package com.funivan.php.extremePhpInspections.inspections
 
-import com.funivan.php.extremePhpInspections.Emojii
 import com.jetbrains.php.lang.inspections.PhpInspection
 
 
-abstract class BaseInspection : PhpInspection() {
-    val emoji = Emojii()
-}
+abstract class BaseInspection : PhpInspection()
